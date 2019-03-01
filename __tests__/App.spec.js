c07340b8df611b83dc32dbc83adec98e0d5e1fdf
NJS <24832578+codenjs@users.noreply.github.com>
2019-02-28 22:55:18 -0500

    Fix timing errors that appeared after unit test execution

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -10,6 +10,10 @@ import App from '../src/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+// Fix errors that get logged after test execution
+// https://github.com/facebook/jest/issues/4359#issuecomment-413238977
+jest.useFakeTimers();
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
