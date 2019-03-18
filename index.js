1082a160ac42ff9a26239cfbc1b59a42bf8b3872
NJS <24832578+codenjs@users.noreply.github.com>
2019-03-17 22:13:28 -0400

    Hide debug warning about Async Storage

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+
+// Ignore the warning until dependency on Async Storage can be removed
+YellowBox.ignoreWarnings(['Async Storage has been extracted']);
 
 AppRegistry.registerComponent(appName, () => App);
